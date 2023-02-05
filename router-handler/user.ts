8cb77523f956020f1be92b2dc361c193a442d0e4
qinzhilong <l905270815@qq.com>
2023-02-05 15:34:51 +0800

    修改密码

--- router-handler/user.ts
+++ router-handler/user.ts
@@ -23,8 +23,12 @@ export const createFn = (req: Request, res: Response) => {
 		};
 		const sql = "insert into user_table set ?";
 		db.query(sql, insertInfo, (err2, results2) => {
-			err2 && res.send({ code: 1, msg: err2.message });
-			results2.affectedRows !== 1 && res.send({ code: 1, msg: "创建用户失败" });
+			if (err2) {
+				return res.send({ code: 1, msg: err2.message });
+			}
+			if (results2.affectedRows !== 1) {
+				return res.send({ code: 1, msg: "创建用户失败" });
+			}
 			// 创建成功
 			res.send({
 				code: 0,
@@ -46,5 +50,43 @@ export const getInfoFn = (req: Request, res: Response) => {
 			return res.send({ code: 1, msg: "查询失败" });
 		}
 		res.send({ code: 0, msg: "获取用户信息成功", data: results[0] });
+	});
+};
+
+// 修改用户密码
+export const updatePasswordFn = (req: Request, res: Response) => {
+	console.log(req.body);
+	const userInfo = req.body;
+	const sqlStr = "select * from user_table where id = ?";
+	db.query(sqlStr, (req as any).user.id, (err, results) => {
+		if (err) return res.send({ code: 1, msg: err.message });
+		if (results.length !== 1) return res.send({ code: 1, msg: "未知用户" });
+		// 判断原密码和数据库中是否一致
+		const finalCompare = bcrypt.compareSync(
+			userInfo.oldpassword,
+			results[0].password
+		);
+		// 判断新密码和确认新密码是否一致
+		const reqCompare = userInfo.password === userInfo.repassword;
+		// 判断新密码和原密码是否一致
+		const compareRes = userInfo.password === userInfo.oldpassword;
+
+		// 新旧密码不一致 并且 新密码和确认密码一致
+		if (finalCompare && reqCompare && !compareRes) {
+			userInfo.password = bcrypt.hashSync(userInfo.password, 5);
+			const sql = "update user_table set password = ? where id = ?";
+			db.query(
+				sql,
+				[userInfo.password, (req as any).user.id],
+				(err2, results2) => {
+					if (err2) return res.send({ code: 1, msg: err2.message });
+					if (results2.affectedRows !== 1)
+						return res.send({ code: 1, msg: "修改失败" });
+					res.send({ code: 0, msg: "修改密码成功" });
+				}
+			);
+		} else {
+			res.send({ code: 1, msg: "原密码错误或确认密码不正确" });
+		}
 	});
 };
--- router/user.ts
+++ router/user.ts
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 // 路由处理函数
-import { createFn, getInfoFn } from "../router-handler/user";
+import { createFn, getInfoFn, updatePasswordFn } from "../router-handler/user";
 
 const router: Router = express.Router();
 
@@ -9,5 +9,8 @@ router.post("/create", createFn);
 
 // 获取用户信息
 router.get("/info", getInfoFn);
+
+// 修改用户密码
+router.post("/updatepassword", updatePasswordFn);
 
 export default router;
